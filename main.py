0afc2d3e904354ec1e6449fead57d8462406fc67
Nathan Le <nathan.le.0326@gmail.com>
2026-04-29 18:20:11 -0400

    CNN model + training + eval

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from medmnist import PneumoniaMNIST
+from medmnist import PneumoniaMNIST, Evaluator
 
 from tqdm import tqdm
 import numpy as np
@@ -14,11 +14,117 @@ data_transform = transforms.Compose([
 ])
 
 trainingData = PneumoniaMNIST(split = "train", download = True, size = 28, transform = data_transform)
-validationData =  PneumoniaMNIST(split = "val", download = True,size = 28, transform = data_transform)
+validationData = PneumoniaMNIST(split = "val", download = True, size = 28, transform = data_transform)
 testData = PneumoniaMNIST(split = "test", download = True, size = 28, transform = data_transform)
 
 trainLoader = data.DataLoader(trainingData, batch_size = 64, shuffle = True)
+trainLoaderEval = data.DataLoader(trainingData, batch_size = 64, shuffle = False)
 valLoader = data.DataLoader(validationData, batch_size = 64, shuffle = False)
 testLoader = data.DataLoader(testData, batch_size = 64, shuffle = False)
 
-trainingData.montage(length=1).save("montage.png")
+task = "binary-class"
+lr = 0.001
+NUM_EPOCHS = 3
+data_flag = "pneumoniamnist" 
+
+# CNN
+class Net(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(Net, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU())
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(16, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.fc = nn.Sequential(
+            nn.Linear(64 * 4 * 4, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes))
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+model = Net(in_channels=1, num_classes=2)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+
+# train
+for epoch in range(NUM_EPOCHS):
+
+    model.train()
+    for inputs, targets in tqdm(trainLoader):
+        # forward + backward + optimize
+        optimizer.zero_grad()
+        outputs = model(inputs)
+
+        targets = targets.squeeze().long()
+        loss = criterion(outputs, targets)
+
+        loss.backward()
+        optimizer.step()
+
+
+# evaluation
+def test(split):
+    model.eval()
+    y_true = torch.tensor([])
+    y_score = torch.tensor([])
+    
+    data_loader = trainLoaderEval if split == 'train' else testLoader
+
+    with torch.no_grad():
+        for inputs, targets in data_loader:
+            outputs = model(inputs)
+
+            targets = targets.squeeze().long()
+            outputs = outputs.softmax(dim=-1)
+            targets = targets.float().resize_(len(targets), 1)
+
+            y_true = torch.cat((y_true, targets), 0)
+            y_score = torch.cat((y_score, outputs), 0)
+
+        y_true = y_true.numpy()
+        y_score = y_score.detach().numpy()
+        
+        evaluator = Evaluator(data_flag, split)
+        metrics = evaluator.evaluate(y_score)
+    
+        print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
+
+print('==> Evaluating ...')
+test('train')
+test('test')
